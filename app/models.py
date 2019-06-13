e55f29f5703b47e7fe13084ed34b421e043cc2a0
AndyHide <andyhidesds@gmail.com>
2019-06-13 12:40:44 +0500

    languages support added

--- app/models.py
+++ app/models.py
@@ -1,13 +1,14 @@
 from datetime import datetime
-from app import db, login, app
-from flask_login import UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
 from hashlib import md5
 from time import time
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
+from app import app, db, login
 
 
-followers = db.Table('followers',
+followers = db.Table(
+    'followers',
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
 )
@@ -35,12 +36,12 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-    
+
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
-    
+
     def follow(self, user):
         if not self.is_following(user):
             self.followed.append(user)
@@ -48,21 +49,23 @@ class User(UserMixin, db.Model):
     def unfollow(self, user):
         if self.is_following(user):
             self.followed.remove(user)
-            
+
     def is_following(self, user):
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
 
     def followed_posts(self):
-        followed = Post.query.join(followers, (followers.c.followed_id == Post.user_id)).filter(followers.c.follower_id == self.id)
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
-    
+
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
             app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
-    
+
     @staticmethod
     def verify_reset_password_token(token):
         try:
@@ -71,7 +74,7 @@ class User(UserMixin, db.Model):
         except:
             return
         return User.query.get(id)
-    
+
 
 @login.user_loader
 def load_user(id):
@@ -83,6 +86,7 @@ class Post(db.Model):
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    language = db.Column(db.String(5))
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
