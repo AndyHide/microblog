2be28cac1ff7b9dbd6dbf80139cd6a1ef473cde0
AndyHide <andyhidesds@gmail.com>
2019-06-17 16:31:09 +0500

    removed translations

--- config.py
+++ config.py
@@ -14,6 +14,5 @@ class Config(object):
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']
     LANGUAGES = ['en', 'es']
-    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     POSTS_PER_PAGE = 25
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
