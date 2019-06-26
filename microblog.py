2701fca9ffb0c674dc1a41718eec1c6fdd18f4ff
AndyHide <andyhidesds@gmail.com>
2019-06-26 11:55:01 +0500

    new template for login page

--- microblog.py
+++ microblog.py
@@ -1,7 +1,7 @@
 from app import app, db
-from app.models import User, Post, Ingredient
+from app.models import User, Post, Ingredient, Recipe
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post, 'Ingredient': Ingredient}
+    return {'db': db, 'User': User, 'Post': Post, 'Ingredient': Ingredient, 'Recipe': Recipe}
