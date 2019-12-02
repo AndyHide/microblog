045ef83ab99d5fc549feb7cbf88aa8d5c26f5a1d
AndyHide <andyhidesds@gmail.com>
2019-12-02 17:01:56 +0500

    fixed winvenv on work

--- winvenv/Scripts/activate.ps1
+++ winvenv/Scripts/activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="c:\python\git\microblog\winvenv"
+$env:VIRTUAL_ENV="C:\Python\git\microblog\winvenv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
